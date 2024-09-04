8826243f319ee95a2cb2a35545e6d644e302560e
Dev Agrawal <agrawaldev261004@gmail.com>
2024-09-04 12:20:12 +0530

    backend and scraper add

---
+++ backend/src/controllers/jobs.controller.js
@@ -0,0 +1,88 @@
+import {Job} from "../models/jobs.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+// Get all jobs
+const getAllJobs = asyncHandler(async(req, res) => {
+    try {
+        const jobs = await Job.find();
+       return res.status(200).json(new ApiResponse(200,jobs,"getting all jobs"));
+    } catch (err) {
+     throw new ApiError(500,'Something went wong while getting all jobs');
+    }
+});
+
+// Get a single job by ID
+const getJobById =asyncHandler( async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        console.log(`Looking for job with ID: ${jobId}`);
+
+       
+        const job = await Job.findById(jobId);
+        console.log(`Query result: ${job}`);
+        if (!job) {
+            console.error(`Job with ID ${jobId} not found`);
+            throw new ApiError(404,"job not found");
+        }
+        return res.status(200).json(new ApiResponse(200,job,"given id job found"));
+        //res.status(200).json(job);
+    } catch (err) {
+        throw new ApiError(500,"error occuring to find one job");;
+    }
+});
+
+// Create a new job
+const createJob = async (req, res) => {
+    try {
+        const newJob = new Job(req.body);
+        const savedJob = await newJob.save();
+        return res.status(201).json(new ApiResponse(201,savedJob,"job created"));
+        //res.status(201).json(savedJob);
+    } catch (err) {
+        throw new ApiError(500,'error creating a new job');
+        //res.status(400).json({ error: err.message });
+    }
+};
+
+// Update a job by ID
+const updateJob = async (req, res) => {
+    try {
+        const updatedJob = await Job.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedJob) {
+            throw new ApiError(404,"job not found so update not done");
+            //return res.status(404).json({ message: 'Job not found' });
+        }
+        return res.status(200).json(new ApiResponse(200,updatedJob,"job updated"));
+       // res.status(200).json(updatedJob);
+    } catch (err) {
+        throw new ApiError(500,'error updating a job');
+       // res.status(400).json({ error: err.message });
+    }
+};
+
+// Delete a job by ID
+const deleteJob = async (req, res) => {
+    try {
+        const deletedJob = await Job.findByIdAndDelete(req.params.id);
+        if (!deletedJob) {
+            throw new ApiError(404,"job not found so delete not done");
+            //return res.status(404).json({ message: 'Job not found' });
+        }
+        return res.status(200).json(new ApiResponse(200,deletedJob,"job deleted"));
+       // res.status(200).json({ message: 'Job deleted successfully' });
+    } catch (err) {
+        throw new ApiError(500,'error deleting a job');
+        //res.status(500).json({ error: err.message });
+    }
+};
+
+export {
+    getAllJobs,
+    getJobById,
+    createJob,
+    updateJob,
+    deleteJob
+    
+}
---
+++ backend/src/routes/jobs.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+const router = Router();
+import { getAllJobs,getJobById,createJob,updateJob,deleteJob } from "../controllers/jobs.controller.js";
+
+// Get all jobs
+router.post('/',getAllJobs);
+//router.get('/', jobsController.getAllJobs);
+
+// Get a single job by ID
+router.route('/id/:id').get(getJobById);
+//router.get('/:id', jobsController.getJobById);
+
+// Create a new job
+router.route('/newJob').post(createJob);
+//router.post('/', jobsController.createJob);
+
+//update job
+router.route("/updateJob/:id").put(updateJob);
+
+//delete job
+router.route('/deleteJob/:id').delete(deleteJob)
+
+export default router
