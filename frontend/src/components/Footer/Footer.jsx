5b599e1e80d87e2e8fc8c37d262a9626a663760c
Sujal <morwanisujal875@gmail.com>
2024-09-05 22:21:26 +0530

    added the logo

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   <div className="absolute -top-10 left-1/2 h-16 w-16 -translate-x-1/2 rounded-xl border-4 border-sky-500 bg-white p-2">
     <img
       className="h-full object-contain"
-      src="/images/logo-circle.png"
+      src="https://res.cloudinary.com/deyfwd4ge/image/upload/v1725554842/Screenshot_2024-09-05_220458_1_1_liojlg.png"
       alt=""
     />
   </div>
